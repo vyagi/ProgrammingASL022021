4650daa73d31b46f8cddc6364cdbdd43ac418ed3
vyagi <marcin.jagiela@hotmail.com>
2021-11-25 17:47:41 +0100

    Comit 2

--- OOP/Basics/Program.cs
+++ OOP/Basics/Program.cs
@@ -5,18 +5,41 @@ namespace Basics
 {
     class Hero
     {
-        public int age;
+        private int age;
 
-        public string name;
+        private string name;
+
+        public Hero(int age, string name)
+        {
+            if (age < 0)
+                throw new ArgumentException("Age cannot be negative");
+
+            if (name == null)
+                throw new ArgumentNullException("Name cannot be null");
+
+            this.age = age;
+            this.name = name;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
     }
     
     class Program
     {
         static void Main(string[] args)
         {
-            Hero hero1 = new Hero();
-            Console.WriteLine(hero1.name);
-            var a = new Animal();
+            Hero hero1 = new Hero(36, "Batman");
+            // hero1.age = -36;
+            // hero1.name = "Batman";
+            Console.WriteLine($"{hero1.GetName()} is {hero1.GetAge()} years old");
         }
     }
 }
